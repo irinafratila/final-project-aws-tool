7ef3d4abcf1c0b59d560d307f5f42effb88ee123
Irina <57362700+irinafratila@users.noreply.github.com>
2023-03-19 17:07:46 +0000

    Backup of web app progress

--- Job/JobTask.py
+++ Job/JobTask.py
@@ -3,27 +3,28 @@ import boto3
 class GetAndStoreDataJob:
 
     def get_instance_and_ami_ids(self, all_instances):
-        instance_id, ami_id, date_launched = "", "", ""
+        instance_data = [] # A list of dictionaries
 
         for r in all_instances['Reservations']:
             for instance in r['Instances']:
-                instance_id = instance['InstanceId']
-                ami_id = instance['ImageId']
-                date_launched = instance['LaunchTime'].strftime("%m/%d/%Y, %H:%M:%S")
+                if instance['State'] == {'Code': 16, 'Name': 'running'}:
+                    instance_id = instance['InstanceId']
+                    ami_id = instance['ImageId']
+                    date_launched = instance['LaunchTime'].strftime("%m/%d/%Y, %H:%M:%S")
+                    instance_data.append({
+                        'instance_id': instance_id,
+                        'ami_id': ami_id,
+                        'date_launched': date_launched
+                    })
 
-        return instance_id, ami_id, date_launched
+        return instance_data
 
-    def save_data(self, table_name, instance_id, ami_id, date_launched):
+    def save_data(self, table_name, data):
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         table = dynamodb.Table(table_name)
 
-        table.put_item(
-            Item={
-                'instance_id': instance_id,
-                'ami_id': ami_id,
-                'date_launched': date_launched
-            }
-        )
+        for item in data:
+            table.put_item(Item=item)
 
 
 if __name__ == "__main__":
@@ -33,8 +34,9 @@ if __name__ == "__main__":
     all_images = ec2.describe_images(Owners=['self'])
 
     worker = GetAndStoreDataJob()
-    instance_id, ami_id, date_launched = worker.get_instance_and_ami_ids(all_instances)
-    worker.save_data('instances-data', instance_id, ami_id, date_launched)
+    data = worker.get_instance_and_ami_ids(all_instances)
+
+    worker.save_data('instances-data',data)
     print("Saved all data successfully!")
 
 #print("===== Custom AMIs =====")
--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import boto3
-from JobTask import GetAndStoreDataJob
+from Job.JobTask import GetAndStoreDataJob
 from moto import *
 
 @mock_dynamodb
@@ -17,7 +17,7 @@ def test_job_task_save_data():
     worker = GetAndStoreDataJob()
     worker.save_data('test', '0012abcd563', '1134abdc789', '04/02/2022, 14:50:10')
 
-    response = table.get_item(Key={instance_id:'0012abcd563'})
+    response = table.get_item(Key={'instance_id':'0012abcd563'})
     output = response['Item']
 
     print(response)
